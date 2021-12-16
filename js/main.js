0bcb6d60edcfd0f2f3ef5a2eb8b4c05ec64fecbc
OUSSADI Karim <oussadikarim00@gmail.com>
2021-12-16 02:18:42 +0100

    seventh commit

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ menu.addEventListener("click", () => {
 close.addEventListener("click", () => {
     menuBar.classList.add("hide")
 })
+
+
+let current = 0;
+for (var i = 0; i < document.links.length; i++) {
+    if (document.links[i].href === document.URL) {
+        current = i;
+    }
+}
+document.links[current].id = 'current';
